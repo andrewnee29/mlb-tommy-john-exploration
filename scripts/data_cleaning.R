3d3781d69cb15428d9b8bd1dc87afa89623e936d
Andrew Nee <andrewnee29@gmail.com>
2025-12-18 16:42:35 -0500

    Update data_cleaning.R

--- scripts/data_cleaning.R
+++ scripts/data_cleaning.R
@@ -1,5 +1,5 @@
 # install.packages("pacman")
-pacman::p_load(dplyr, baseballr, readr, lubridate)
+pacman::p_load(dplyr, baseballr, readr, lubridate, tidyr)
 
 # read in raw tommy john surgery list file
 tj_data_raw <- read_csv("../data/tj_surgery_list.csv")
@@ -20,10 +20,24 @@ tj_data <- tj_data %>%
 tj_data <- tj_data %>%
   filter(return_year >= 2018 & return_year <= 2023)
 
-# only relevant columns
+# split player name into first and last
 tj_data <- tj_data %>%
-  select(Player, mlbamid, fgid, Team, Throws, Country, `High School`, `College(s)`, Age, surgery_date, surgery_year, return_date, return_year, `Surgeon(s)`)
+  separate(Player, 
+           into = c("first_name", "last_name"), 
+           sep = " ", 
+           remove = FALSE)  # keep original column
 
+# only pitchers that have returned
 tj_data <- tj_data %>%
   filter(!is.na(return_date))
 
+# player id joins ----
+# player_ids <- chadwick_player_lu()
+
+# Left join using mlbamid and key_mlbam
+tj_data <- tj_data %>%
+  left_join(player_ids, by = c("mlbamid" = "key_mlbam"))
+
+# only relevant columns
+tj_data <- tj_data %>%
+  select(Player, name_first, name_last, mlbamid, key_fangraphs, Team, Throws, Country, Age, surgery_date, surgery_year, return_date, return_year, `Surgeon(s)`)
